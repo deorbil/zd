ba3362a1e3d963dcd073bf64f499ea84d385fc0c
deorbil <80085032+deorbil@users.noreply.github.com>
2025-04-21 10:32:08 +0700

    refactor: move rc fallback to inside zd function and replace it with bash default value substitution

--- zd.sh
+++ zd.sh
@@ -1,12 +1,12 @@
-[[ -z $ZD_RC ]] && ZD_RC="$HOME/.zdrc.sh"
-
 # Ask user to select a directory from user defined list
 zd() {
-  [[ -f $ZD_RC ]] || return
+  local rc=${ZD_RC:-$HOME/.zdrc.sh}
+
+  [[ -f $rc ]] || return
 
   local dirs
   # shellcheck disable=SC1090
-  dirs=$(source "${ZD_RC}")
+  dirs=$(source "$rc")
 
   echo "$dirs" | fzf --print-query | tail -1
 }
