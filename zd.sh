d5bc61d129595310584f2f44d3511bbc222efd47
deorbil <80085032+deorbil@users.noreply.github.com>
2025-04-21 10:56:35 +0700

    refactor: extract plugin sourcing into function

--- zd.sh
+++ zd.sh
@@ -1,3 +1,18 @@
+# Source a plugin
+_zd_plugin() {
+  local plugin="$ZD_DIR/plugins/$1.sh"
+  # shellcheck disable=SC1090
+  [[ -f $plugin ]] && source "$plugin"
+}
+
+# Source enabled plugins
+_zd_plugins() {
+  local plugin
+  for plugin in "${ZD_PLUGINS[@]}"; do
+    _zd_plugin "$plugin"
+  done
+}
+
 # Ask user to select a directory from user defined list
 zd() {
   local rc=${ZD_RC:-$HOME/.zdrc.sh}
@@ -11,11 +26,4 @@ zd() {
   echo "$dirs" | fzf --print-query | tail -1
 }
 
-# Source enabled plugins
-for plugin in "${ZD_PLUGINS[@]}"; do
-  if [[ -f $ZD_DIR/plugins/$plugin.sh ]]; then
-    # shellcheck disable=SC1090
-    source "$ZD_DIR/plugins/$plugin.sh"
-  fi
-done
-unset plugin
+_zd_plugins
