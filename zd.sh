3cb599081a3ca9ba7c036bd8b82d01f6cc1be736
deorbil <80085032+deorbil@users.noreply.github.com>
2025-04-21 08:05:29 +0700

    feat: source plugins

--- zd.sh
+++ zd.sh
@@ -10,3 +10,12 @@ zd() {
 
   echo "$dirs" | fzf --print-query | tail -1
 }
+
+# Source enabled plugins
+for plugin in "${ZD_PLUGINS[@]}"; do
+  if [[ -f $ZD_DIR/plugins/$plugin.sh ]]; then
+    # shellcheck disable=SC1090
+    source "$ZD_DIR/plugins/$plugin.sh"
+  fi
+done
+unset plugin
