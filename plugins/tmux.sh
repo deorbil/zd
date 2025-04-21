40afedfd2cef181606ef430785453dc38d9f94b8
deorbil <80085032+deorbil@users.noreply.github.com>
2025-04-21 08:06:04 +0700

    feat: add tmux plugin

---
+++ plugins/tmux.sh
@@ -0,0 +1,23 @@
+zd_tmux() {
+  local dir
+  dir=$(zd)
+
+  [[ -z $dir ]] && return
+
+  local name
+  name=$(basename "$dir" | tr . _)
+
+  # Create session if it doesn't exist
+  if ! tmux has-session -t "$name" 2>/dev/null; then
+    tmux new-session -s "$name" -c "$dir" -d
+  fi
+
+  # Attach or switch to session if already in tmux to prevent nesting
+  if [[ -n $TMUX ]]; then
+    tmux switch-client -t "$name"
+  else
+    tmux attach-session -t "$name"
+  fi
+}
+
+[[ $ZD_DISABLE_ALIAS = "true" ]] || alias zt=zd_tmux
