2c7664170aa74d2a6bea56a1cfc5033ef6b4e0a8
deorbil <80085032+deorbil@users.noreply.github.com>
2025-07-12 18:15:15 +0700

    refactor: add get plugin dir function

--- src/utils/env.rs
+++ src/utils/env.rs
@@ -6,3 +6,7 @@ pub fn get_zd_dir() -> Result<PathBuf, VarError> {
         .map(PathBuf::from)
         .or_else(|_| std::env::var("HOME").map(|dir| PathBuf::from(dir).join(".zd")))
 }
+
+pub fn get_plugins_dir() -> Result<PathBuf, VarError> {
+    get_zd_dir().map(|dir| dir.join("plugins"))
+}
