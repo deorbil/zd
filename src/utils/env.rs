ef944ed8d32f0d080b2ca2f084997e3154abf18d
deorbil <80085032+deorbil@users.noreply.github.com>
2025-08-25 18:18:02 +0700

    refactor: utils anyhow result

--- src/utils/env.rs
+++ src/utils/env.rs
@@ -1,12 +1,14 @@
-use std::env::VarError;
 use std::path::PathBuf;
 
-pub fn get_zd_dir() -> Result<PathBuf, VarError> {
+use anyhow::{Error, Result};
+
+pub fn get_zd_dir() -> Result<PathBuf> {
     std::env::var("ZD_DIR")
         .map(PathBuf::from)
         .or_else(|_| std::env::var("HOME").map(|dir| PathBuf::from(dir).join(".zd")))
+        .map_err(Error::from)
 }
 
-pub fn get_plugins_dir() -> Result<PathBuf, VarError> {
+pub fn get_plugins_dir() -> Result<PathBuf> {
     get_zd_dir().map(|dir| dir.join("plugins"))
 }
