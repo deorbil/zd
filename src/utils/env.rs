b3bb9cbe4ef734652e62574313c123f94a87cf5d
deorbil <80085032+deorbil@users.noreply.github.com>
2025-08-25 18:18:02 +0700

    refactor: replace map with match in env

--- src/utils/env.rs
+++ src/utils/env.rs
@@ -1,12 +1,15 @@
-use std::env::VarError;
+use anyhow::Result;
 use std::path::PathBuf;
 
-pub fn get_zd_dir() -> Result<PathBuf, VarError> {
-    std::env::var("ZD_DIR")
-        .map(PathBuf::from)
-        .or_else(|_| std::env::var("HOME").map(|dir| PathBuf::from(dir).join(".zd")))
+pub fn get_zd_dir() -> Result<PathBuf> {
+    let dir = match std::env::var("ZD_DIR") {
+        Ok(dir) => PathBuf::from(dir),
+        Err(_) => PathBuf::from(std::env::var("HOME")?).join(".zd"),
+    };
+    Ok(dir)
 }
 
-pub fn get_plugins_dir() -> Result<PathBuf, VarError> {
-    get_zd_dir().map(|dir| dir.join("plugins"))
+pub fn get_plugins_dir() -> Result<PathBuf> {
+    let dir = get_zd_dir()?.join("plugins");
+    Ok(dir)
 }
