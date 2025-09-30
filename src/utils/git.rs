1e83da42e437a1307111c1557131c367546e43cc
deorbil <80085032+deorbil@users.noreply.github.com>
2025-09-30 20:41:33 +0700

    test: get name from url

--- src/utils/git.rs
+++ src/utils/git.rs
@@ -6,3 +6,38 @@ pub fn get_name_from_url(url: &str) -> String {
         .unwrap()
         .to_string()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_name_from_https() {
+        let name = get_name_from_url("https://github.com/deorbil/zd-cd.git");
+        assert_eq!(name, "zd-cd");
+    }
+
+    #[test]
+    fn get_name_from_https_with_trailing_slash() {
+        let name = get_name_from_url("https://github.com/deorbil/zd-cd/");
+        assert_eq!(name, "zd-cd");
+    }
+
+    #[test]
+    fn get_name_from_https_without_suffix() {
+        let name = get_name_from_url("https://github.com/deorbil/zd-cd");
+        assert_eq!(name, "zd-cd");
+    }
+
+    #[test]
+    fn get_name_from_ssh() {
+        let name = get_name_from_url("git@github.com:deorbil/zd-cd.git");
+        assert_eq!(name, "zd-cd");
+    }
+
+    #[test]
+    fn get_name_from_ssh_without_suffix() {
+        let name = get_name_from_url("git@github.com:deorbil/zd-cd");
+        assert_eq!(name, "zd-cd");
+    }
+}
