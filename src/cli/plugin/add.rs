9401846493b1ca73feb3594049dece70a0e8d1e9
deorbil <80085032+deorbil@users.noreply.github.com>
2025-09-30 17:52:22 +0700

    refactor: use utils

--- src/cli/plugin/add.rs
+++ src/cli/plugin/add.rs
@@ -2,8 +2,7 @@ use anyhow::Result;
 use clap::Parser;
 use git2::Repository;
 
-use crate::utils::env;
-use crate::utils::git;
+use crate::utils;
 
 #[derive(Parser)]
 pub struct Add {
@@ -12,8 +11,8 @@ pub struct Add {
 
 impl Add {
     pub fn run(&self) -> Result<()> {
-        let name = git::get_name_from_url(&self.url);
-        let dir = env::get_plugins_dir()?.join(name);
+        let name = utils::git::get_name_from_url(&self.url);
+        let dir = utils::env::get_plugins_dir()?.join(name);
         std::fs::create_dir_all(&dir)?;
         Repository::clone(&self.url, &dir)?;
         Ok(())
