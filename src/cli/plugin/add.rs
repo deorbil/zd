a44ab04db8f8b189e799063e26f26c482c3a8a91
deorbil <80085032+deorbil@users.noreply.github.com>
2025-09-30 20:32:52 +0700

    feat(cli): add plugin from github slug

--- src/cli/plugin/add.rs
+++ src/cli/plugin/add.rs
@@ -12,9 +12,10 @@ pub struct Add {
 impl Add {
     pub fn run(&self) -> Result<()> {
         let name = utils::git::get_name_from_url(&self.url);
+        let url = utils::git::normalize_url(&self.url);
         let dir = utils::env::get_plugin_dir(&name)?;
         std::fs::create_dir_all(&dir)?;
-        Repository::clone(&self.url, &dir)?;
+        Repository::clone(&url, &dir)?;
         Ok(())
     }
 }
