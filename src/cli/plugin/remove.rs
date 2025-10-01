a1cf92cfbb20afcd0571beea87183c3f0459b99e
deorbil <80085032+deorbil@users.noreply.github.com>
2025-10-02 00:36:03 +0700

    refactor: get plugin dir util

--- src/cli/plugin/remove.rs
+++ src/cli/plugin/remove.rs
@@ -10,7 +10,7 @@ pub struct Remove {
 
 impl Remove {
     pub fn run(&self) -> Result<()> {
-        let dir = utils::env::get_plugins_dir()?.join(&self.name);
+        let dir = utils::env::get_plugin_dir(&self.name)?;
         std::fs::remove_dir_all(&dir)?;
         Ok(())
     }
